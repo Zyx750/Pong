1477a8bba8fce6388f638a9012ce987730cc4f92
Zyx750 <maciej.trzaskacz@gmail.com>
2024-02-19 19:00:08 +0100

    basic pausing

--- Main.cs
+++ Main.cs
@@ -1,12 +1,26 @@
+using System.Diagnostics.Contracts;
 using Godot;
 
 public partial class Main : Node
 {
 	Game game;
+	bool paused = false;
 	public void OnStartGame(bool vsCpu) {
 		game = ResourceLoader.Load<PackedScene>("res://game.tscn").Instantiate() as Game;
 		AddChild(game);
 		GetNode<Menu>("MainMenu").QueueFree();
 		game.StartGame(vsCpu);
+	}
+
+	public override void _UnhandledInput(InputEvent @event) {
+		if(@event.IsActionPressed("pause")) {
+			if(GetTree().Paused) {
+				GetTree().Paused = false;
+			}
+			else {
+				GetTree().Paused = true;
+			}
+			GetNode<Timer>("PauseTimer").Start();
+		}
 	}
 }
